0d9c699118ddacb6d8595839b2bef1d2f4a77571
precious <ikegboprecious2000@gmail.com>
2022-10-31 01:36:06 -0700

    made some changes

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -6,12 +6,12 @@ import git from "../Images/Icon.png"
 function Links() {
   return (
     <div className='links'>
-    <a href='https://twitter.com/NgoziChukwuu?t=zu57Bs4-NtM4EdFJy4ZLtg&s=09' id=''>Twitter Link</a>
-    <a href='https://training.zuri.team/' id='btn_zuri'>Zuri Team</a>
-    <a href='http://books.zuri.team' id='books'>Zuri Books</a>
-    <a href='https://books.zuri.team/python-for-beginners?ref_id=<preciousikegbo>' id='book_python'>Python Books</a>
-    <a href='https://background.zuri.team' id='pitch'>Background Check for Coders</a>
-    <a href='https://books.zuri.team/design-rules' id='book_design'>Design Books</a>
+    <a href='https://twitter.com/NgoziChukwuu?t=zu57Bs4-NtM4EdFJy4ZLtg&s=09' target="_blank">Twitter Link</a>
+    <a href='https://training.zuri.team/' id='btn_zuri' target="_blank">Zuri Team</a>
+    <a href='http://books.zuri.team' id='books' target="_blank">Zuri Books</a>
+    <a href='https://books.zuri.team/python-for-beginners?ref_id=<preciousikegbo>' id='book_python' target="_blank">Python Books</a>
+    <a href='https://background.zuri.team' id='pitch' target="_blank">Background Check for Coders</a>
+    <a href='https://books.zuri.team/design-rules' id='book_design' target="_blank">Design Books</a>
        <div className='icons'>
        <div className='social-icons'>
        <img src={slack} alt='slack icon'/>
