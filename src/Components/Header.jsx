c98d79dfbff99d0f2d0594b6f393147087fe1d92
precious <ikegboprecious2000@gmail.com>
2022-10-30 23:16:13 -0700

    added new components

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import "./Linktreestyles.css"
 
-import profile from "../images/Snapchat-680063175 (2).jpg"
-import share from "../images/_Avatar share button (1).png"
-import sharebtn from "../images/_Avatar share button.png"
+import profile from "../Images/Snapchat-680063175 (2).jpg"
+import share from "../Images/_Avatar share button (1).png"
+import sharebtn from "../Images/_Avatar share button.png"
 
 function Header() {
   return (
-    <div>
+    <div className='body'>
+    <div className='header'>
+    <img  id='profile_img' src={profile} alt="profile-img"/>
+      <h4 id='twitter'>Ngozi</h4>
+      <h4 id='slack'>precious ikegbo</h4>
+      <div className='avatar'>
+      <img className= "avatar-sharearrow" src={share}   alt='avatar share'/>
+      <img className= "avatar-sharebtn" src={sharebtn}   alt='avatar share'/>
+      </div>
       
+    </div>
     </div>
   )
 }
--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import "./Linktreestyles.css"
-import slack from "../images/slack.png"
-import git from "../images/Icon.png"
+import slack from "../Images/slack.png"
+import git from "../Images/Icon.png"
 
 function Links() {
   return (
-    <div>
+    <div className='links'>
+    <a href='https://twitter.com/NgoziChukwuu?t=zu57Bs4-NtM4EdFJy4ZLtg&s=09' id=''>Twitter Link</a>
+    <a href='https://training.zuri.team/' id='btn_zuri'>Zuri Team</a>
+    <a href='http://books.zuri.team' id='books'>Zuri Books</a>
+    <a href='https://books.zuri.team/python-for-beginners?ref_id=<preciousikegbo>' id='book_python'>Python Books</a>
+    <a href='https://background.zuri.team' id='pitch'>Background Check for Coders</a>
+    <a href='https://books.zuri.team/design-rules' id='book_design'>Design Books</a>
+       <div className='icons'>
+       <div className='social-icons'>
+       <img src={slack} alt='slack icon'/>
+       <img src={git} alt="git icon"/>
+       </div>
+     
+       </div>
+
+      
       
     </div>
   )
